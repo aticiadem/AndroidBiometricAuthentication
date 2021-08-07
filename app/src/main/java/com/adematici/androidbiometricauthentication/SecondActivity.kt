66170e99142697b73f66e83447af95af0df28be6
adematici <adematicis41@gmail.com>
2021-08-07 19:36:47 +0300

    activities design completed

--- app/src/main/java/com/adematici/androidbiometricauthentication/SecondActivity.kt
+++ app/src/main/java/com/adematici/androidbiometricauthentication/SecondActivity.kt
@@ -2,15 +2,15 @@ package com.adematici.androidbiometricauthentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.adematici.androidbiometricauthentication.databinding.ActivityMainBinding
+import com.adematici.androidbiometricauthentication.databinding.ActivitySecondBinding
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivitySecondBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
